476c698a50d57fbec8a0248444633d0ac26f5f71
Lonni Robinson <lonnirobinson@Lonnis-MacBook-Pro.local>
2022-07-31 19:39:14 -0400

    adding more questions

--- P1BG.js
+++ P1BG.js
@@ -1,17 +1,121 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-function startGame(){
-    console.log('Started')
+let shuffledQuestions, currentQuestionIndex
 
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-
-
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
-function setNextquestion(){
 
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
-function selectAnswer(){
 
-
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+      question: 'who held naruto hostage when he was born?',
+      answers: [
+        { text: 'Madara', correct: true },
+        { text: 'He was not a hostage', correct: false },
+        { text: 'Skura', correct: false }
+        
+    
+      ]
+    },
+    {
+      question: 'Whom did Kakashi get his eye from',
+      answers: [
+        { text: 'obito', correct: true },
+        { text: 'Naruto', correct: false},
+        { text: 'itachi', correct: false },
+        { text: 'bobby', correct: false }
+      ]
+    },
+    {
+      question: 'How many times did Naruto fail the graduation test ',
+      answers: [
+        { text: 'once', correct: false },
+        { text: 'three', correct: true },
+        { text: 'four', correct: false },
+        { text: 'zero', correct: false }
+      ]
+    },
+    {
+      question: 'Naruto is a container of which Tailed-Beast?',
+      answers: [
+        { text: 'six-tails', correct: false },
+        { text: 'eight-tails', correct: true},
+        { text: 'one-tails', correct: false }
+      ]
+    }
+  ]
+
+
+
+
+
